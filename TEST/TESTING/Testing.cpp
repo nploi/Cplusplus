9f4539a04fdaef0b3681ae4ff13edf0e7a87af80
nploi <nploi1998@gmail.com>
2018-02-02 13:41:04 +0700

    Update TEST for unit-test

---
+++ TEST/TESTING/Testing.cpp
@@ -0,0 +1,32 @@
+//
+// Created by loinp on 02/02/2018.
+//
+
+#include "Testing.h"
+
+using testing::Eq;
+
+namespace {
+    class ClassDeclaration : public testing::Test{
+    public:
+        Testing obj;
+        ClassDeclaration(){
+            obj;
+
+        }
+
+    };
+}
+
+TEST(TESTCASE01, ClassDeclaration) {
+    //expectedResut = result you need
+    //actualResult = FUNC
+    //ASSERT_EQ(exceptedResult, actualResult);
+
+    ASSERT_EQ(1, 1);
+}
+
+TEST(TESTCASE02, ClassDeclaration){
+
+    ASSERT_EQ(1, 1);
+}
---
+++ TEST/TESTING/Testing.h
@@ -0,0 +1,16 @@
+//
+// Created by loinp on 02/02/2018.
+//
+
+#ifndef CODING_TESTING_H
+#define CODING_TESTING_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class Testing {
+
+};
+
+
+#endif //CODING_TESTING_H
