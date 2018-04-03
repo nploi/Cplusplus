5a36b1a0295297fe81588030be72e1f835f49c12
nploi <nploi1998@gmail.com>
2018-04-03 23:53:24 +0700

    Change directory

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+
     testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
     return 0;
